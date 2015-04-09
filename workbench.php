89570ea4ffb9c0bc8a2993ca16e2b4d764e84ec9
Thad Bryson <tbryson@bcgtrans.com>
2015-04-09 17:17:48 -0400

    starting next version

--- workbench.php
+++ workbench.php
@@ -2,19 +2,18 @@
 
 set_time_limit(0);
 
-require __DIR__."/vendor/autoload.php";
+require __DIR__.'/vendor/autoload.php';
 
-use TCB\SantasWorkshop\Command;
-use TCB\SantasWorkshop\Command\CreateCommand;
-use TCB\SantasWorkshop\Command\BuildCommand;
+use TCB\Santa\Command\CreateCommand;
+use TCB\Santa\Command\BuildCommand;
 
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Shell;
 
-define("BASE_DIR", __DIR__);
+define('BASE_DIR', __DIR__);
 
 // Setup Application.
-$app = new Application("Santa's Workshop", "v2");
+$app = new Application('Santa\'s Workshop', 'v2');
 
 // Add commands to Application.
 $app->add(new CreateCommand());
